23a1eac91a7dd3e38f6cceecc632af09b4ffc33c
DzmitrySakalouski <dmitry89sokol@gmail.com>
2020-12-11 18:39:33 +0300

    add toolbar on home screen

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,4 +1,5 @@
 export const COLORS = {
     BLUE: "#39C2D7",
-    DARK: "#464547"
-}
+    DARK: "#464547",
+    RED: "#B22746",
+};
--- src/views/HomeDashboard/home_dashboard.view.jsx
+++ src/views/HomeDashboard/home_dashboard.view.jsx
@@ -1,18 +1,81 @@
-import React, { useState } from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {getUserPersonalData} from '../../store/actions/User/actions/user.actions';
+import {
+  AppBar,
+  IconButton,
+  makeStyles,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { COLORS } from "../../constants/colors";
+import { getUserPersonalData } from "../../store/actions/User/actions/user.actions";
 
-export const HomeDashboard = props => {
-    const {user} = useSelector(state => state.user);
-    const dispatch = useDispatch();
+const useStyles = makeStyles({
+  title: {
+    flexGrow: 1,
+  },
+  toolbar: {
+    backgroundColor: COLORS.DARK,
+  },
+  title: {
+    fontWeight: "bold",
+    flexGrow: 1,
+  },
+  logOutMenuItem: {
+      color: COLORS.RED,
+      textTransform: "uppercase",
+  }
+});
 
-    useState(() => {
-        dispatch(getUserPersonalData(user?.uid));
-    }, []);
+export const HomeDashboard = (props) => {
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-    return (
-        <div>
-            Homejk;jlkj
-        </div>
-    )
-}
+  useState(() => {
+    dispatch(getUserPersonalData(user?.uid));
+  }, []);
+
+  const toggleMenu = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+      console.log("LOGOUT");
+      handleClose();
+  }
+
+  return (
+    <AppBar position="static">
+      <Toolbar classes={{ root: classes.toolbar }}>
+        <Typography className={classes.title}>JAM STUDIOS</Typography>
+        <IconButton color="inherit" onClick={toggleMenu}>
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          keepMounted
+          transformOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Аккаунт</MenuItem>
+          <MenuItem onClick={handleLogout} className={classes.logOutMenuItem}>Выйти</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+};
